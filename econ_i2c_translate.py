6fe7932bdf4f997fbebd0b2f6154ac0cb8c003b9
cmantill <criss.ms7@gmail.com>
2021-01-25 12:13:06 -0600

    add tester for board

---
+++ econ_i2c_translate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#from econ_i2c import econ_i2c
+from translator import Translator
+
+#i2c = econ_i2c(1)
+
+# Define pairs from Translator
+translator = Translator()
+writeCaches = {}
+pairs = translator.pairs_from_cfg(translator.paramMap['econ'],writeCaches)
+writeCaches.update(pairs)
+
+# Write and Read
+for addr,val in writeCaches.items():
+    i2c.write(0x20, addr, val)
+    readback = i2c.read(0x20, addr, 1)
+    print("0x%02x"%tuple(readback))
--- translator.py
+++ translator.py
@@ -5,16 +5,16 @@ class Translator():
     """ Translate between (human-readable) config and corresponding address/register values. """
 
     def __init__(self):
-         self.paramMap = self.__load_param_map("reg_maps/ECON_I2C_params_regmap.yaml")
-         self.pairs_from_cfg(self.paramMap['econ'])
-         
+        self.paramMap = self.__load_param_map("reg_maps/ECON_I2C_params_regmap.yaml")
+        #self.pairs_from_cfg(self.paramMap['econ'],{})
+
     def __load_param_map(self, fname):
         """ Load a yaml register map into cache (as a dict). """
         with open(fname) as fin:
             paramMap = safe_load(fin)
         return paramMap
 
-    def pairs_from_cfg(self, cfg):
+    def pairs_from_cfg(self, cfg, writeCache):
         """
         Convert an input config dict to address: value pairs
         Case 1: One parameter value has one register
@@ -26,27 +26,43 @@ class Translator():
         for block in cfg:
             for access in cfg[block]:
                 for param, paramDict in  cfg[block][access].items():
-                    addr = paramDict['register'] + paramDict['reg_offset']
-                    if addr in pairs:
-                        if paramDict['size_byte'] > 1: prev_regVal =int.from_bytes(pairs[addr], 'little')
-                        else: prev_regVal = pairs[addr][0]
+                    address = paramDict['register'] + paramDict['reg_offset']
+                    values = paramDict['default']
+                    if 'reg_shift' in paramDict:
+                        addrList = [address + i*paramDict['reg_shift'] for i in range(len(values))]
+                        try:
+                            valList = values
+                        except:
+                            print('list of addresses but not values')
                     else:
-                        prev_regVal = 0
-                    # convert parameter value (from config) into register value
-                    paramVal = paramDict['default']
-                    if 'param_mask' in paramDict:
-                        paramVal = paramVal & paramDict['param_mask']
-                    if 'param_shift' in paramDict:
-                        paramVal <<= paramDict['param_shift']
-                    val = prev_regVal + paramVal
+                        addrList = [address]
+                        valList = [values]
+                    for i,addr in enumerate(addrList):
+                        # previous register value
+                        if addr in pairs:
+                            if paramDict['size_byte'] > 1: prev_regVal = int.from_bytes(pairs[addr], 'little')
+                            else: prev_regVal = pairs[addr][0]
+                        elif addr in writeCache:
+                            prev_paramVal = writeCache[addr][0]
+                        else:
+                            prev_regVal = 0
+                            
+                        # convert parameter value (from config) into register value
+                        paramVal = valList[i]
+                        if 'param_mask' in paramDict: paramVal = paramVal & paramDict['param_mask']
+                        if 'param_shift' in paramDict: paramVal <<= paramDict['param_shift']
+                        val = prev_regVal + paramVal
+                        if paramDict['size_byte'] > 1:
+                            pairs[addr] = list(val.to_bytes(paramDict['size_byte'], 'little'))
+                        else:
+                            pairs[addr] = [val]
 
-                    if paramDict['size_byte'] > 1:
-                        pairs[addr] = list(val.to_bytes(paramDict['size_byte'], 'little'))
-                    else:
-                        pairs[addr] = [val]
-
+        # testing
+        '''
         for p,lpp in pairs.items():
+            print(p,lpp)
             for pp in lpp:
                 print(hex(p), hex(pp))
-
-Translator()
+        '''
+        return pairs
+    
