5d2b5f3517483386aebf1e7d512dee30b539de5e
cmantill <criss.ms7@gmail.com>
2021-02-09 16:00:45 -0600

    new values from test map

--- econ_i2c_translate.py
+++ econ_i2c_translate.py
@@ -6,14 +6,21 @@ from translator import Translator
 #i2c = econ_i2c(1)
 
 # Define pairs from Translator
-translator = Translator()
+translator = Translator('ECON-T')
 writeCaches = {}
-pairs = translator.pairs_from_cfg(translator.paramMap['ECON-T'],writeCaches)
+pairs = translator.pairs_from_cfg(translator.paramMap,writeCaches)
 writeCaches.update(pairs)
 
+# config from pairs
+#translator.cfg_from_pairs(writeCaches)
+
+# read other parammap
+paramMap = translator.load_param_map("reg_maps/ECON_I2C_params_test.yaml")['ECON-T']
+translator.pairs_from_cfg(paramMap,writeCaches)
+
 # Write and Read
-for addr,val in writeCaches.items():
-    print(addr, val) 
-    #i2c.write(0x20, addr, val)
-    #readback = i2c.read(0x20, addr, 1)
-    #print("0x%02x"%tuple(readback))
+#for addr,val in writeCaches.items():
+#    print(addr, val) 
+#i2c.write(0x20, addr, val)
+#readback = i2c.read(0x20, addr, 1)
+#print("0x%02x"%tuple(readback))
--- translator.py
+++ translator.py
@@ -16,10 +16,14 @@ def memoize(fn):
 class Translator():
     """ Translate between (human-readable) config and corresponding address/register values. """
 
-    def __init__(self):
-        self.paramMap = self.__load_param_map("reg_maps/ECON_I2C_params_regmap.yaml")
+    def __init__(self,econ):
+        self.econ = econ
+        self.paramMap = self.load_param_map("reg_maps/ECON_I2C_params_regmap.yaml")[self.econ]
+        self.regDict = {}
+        self.nInputChannels = self.paramMap['ninput']
+        self.nOutputChannels = self.paramMap['noutput']
 
-    def __load_param_map(self, fname):
+    def load_param_map(self, fname):
         """ Load a yaml register map into cache (as a dict). """
         with open(fname) as fin:
             paramMap = safe_load(fin)
@@ -29,12 +33,16 @@ class Translator():
         """
         Convert from {addr:val} pairs to {param:param_val} config.
         We can only recover a parameter from a pair when it is in the common cache.
-        However, when we read (or write) a param the common cache is populated in advance.
         """
         cfg = nested_dict()
-        #print(self.__regs_from_paramMap.items())
-        for param, param_regs in self.__regs_from_paramMap.cache.items():
-            print(param,param_regs)
+        for access, accessDict in self.__regs_from_paramMap.cache.items():
+            for block, blockDict in accessDict.items():
+                for param, paramDict in blockDict.items():
+                    addr = paramDict['addr']
+                    prev_regVal = cfg[access][block][param] if cfg[access][block][param]!={} else 0
+                    paramVal = int.from_bytes(pairs[addr], 'little')
+                    cfg[access][block][param] = paramVal
+                    
         return cfg.to_dict()
 
     def pairs_from_cfg(self, cfg, writeCache):
@@ -42,91 +50,133 @@ class Translator():
         Convert an input config dict to address: value pairs
         Case 1: One parameter value has one register
         Case 2: Several parameter values share same register
-        Case 3: A block of registers is `repeated` for the number of channels (*)
-        Case 4: A block of paramaters for a given register is `repeated` for the number of channels (*)
+        Case 3: A block of registers is `repeated` for the number of input channels (*)
+        Case 4: A block of parameters for a given register is `repeated` for the number of input channels (*)
         """
         cfg_str = dump(cfg)
         pairs = {}
-        nchannels = cfg['nchannels']
         for access in cfg:
-            if access=='nchannels': continue
-            for block in cfg[access]:
-                block_shift = cfg[access][block]['block_shift'] if 'block_shift' in cfg[access][block] else 0
-                addr_base = cfg[access][block]['addr_base']
-                    
-                for param, paramDict in  cfg[access][block]['params'].items():
-                    #print(access,block,param,paramDict)
-                    addr_offset = paramDict['addr_offset'] if 'addr_offset' in paramDict else 0
-                    size_byte = paramDict['size_byte'] if 'size_byte' in paramDict else 1
-                    
-                    address = addr_base + addr_offset
-                    values = paramDict['default']
-                    
-                    # make list of addresses and values
-                    if '*' in param:
-                        try:
-                            #paramList = [param.replace('*',i) for i in range(len(values))]
-                            addrList = [address + i*paramDict['addr_shift'] for i in range(len(values))]
-                            valList = values
-                        except:
-                            print('list of parameters but no shift of address')
-                            raise
-                    elif '*' in block:
-                        try:
-                            #paramList[block] = [param]
-                            addrList = [address + i*block_shift for i in range(nchannels)]
-                            valList = [values for i in range(nchannels)]
-                        except:
-                            print('no block shift in block dictionary')
-                            raise
+            if access=='ninput' or access=='noutput': continue
+            # should replace this w. config
+            par_regs = self.__regs_from_paramMap(access)
+            for block,blockDict in par_regs.items():
+                for param, paramDict in blockDict.items():
+                    addr = paramDict['addr']
+                    size_byte = paramDict['size_byte']
+
+                    # check if values are given in the config - otherwise they are taken from the default map
+                    paramVal = paramDict['val']
+                    if block in cfg[access] and addr in writeCache:
+                        prev_regVal = writeCache[addr][0]
+                        if param in cfg[access][block]['registers']:
+                            reg = cfg[access][block]['registers'][param]
+                            if 'value' in reg: paramVal = reg['value']
+                            if 'params' in reg: 
+                                tmpparamDict = paramDict['params']
+                                for par, parreg in reg['params'].items():
+                                    tmpparamDict[par]['param_value'] = parreg['param_value']
+                                paramVal = self.__regVal_from_paramValues(tmpparamDict)
+                    #elif addr in pairs:
+                    #    prev_regVal = int.from_bytes(pairs[addr], 'little')
+                    #else:
+                    #    prev_regVal = 0
+                        
+                    #print(param,block,paramVal,prev_regVal)
+                    # convert parameter value (from config) into register value
+                    #val = prev_regVal + paramVal
+                    val = paramVal
+                    if size_byte > 1:
+                        pairs[addr] = list(val.to_bytes(size_byte, 'little'))
                     else:
-                        addrList = [address]
-                        valList = [values]
+                        pairs[addr] = [val]
 
-                    par_regs = self.__regs_from_paramMap(access, block, param)
-                    #print('par_regs ',par_regs)
-                    for i,addr in enumerate(addrList):
-                        # check for previous register value
-                        if addr in pairs:
-                            if size_byte > 1: prev_regVal = int.from_bytes(pairs[addr], 'little')
-                            else: prev_regVal = pairs[addr][0]
-                        elif addr in writeCache:
-                            prev_paramVal = writeCache[addr][0]
-                        else:
-                            prev_regVal = 0
-                            
-                        # convert parameter value (from config) into register value
-                        paramVal = valList[i]
-                        val = prev_regVal + paramVal
-                        if size_byte > 1:
-                            pairs[addr] = list(val.to_bytes(size_byte, 'little'))
-                        else:
-                            pairs[addr] = [val]
-
-        # testing
-        '''
-        for p,lpp in pairs.items():
-            print(p,lpp)
-            for pp in lpp:
-                print(hex(p), hex(pp))
-                '''
-        print(self.paramMap['ECON-T']['RW']['PLL_ALL']['params'])
         return pairs
-    
+
     @memoize
-    def __regs_from_paramMap(self, access, block, param):
-        return self.paramMap['ECON-T'][access][block]['params'][param]
+    def __regs_from_paramMap(self, access):
+        """ Expand json parameter Map into full dictionary of:
+            'register': {'addr': X,
+                         'size_byte': X,
+                         'value': X}
+        """
+        self.regDict[access] = {}
+        for block,blockDict in self.paramMap[access].items():
+            block_shift = blockDict['block_shift'] if 'block_shift' in blockDict else 0
+            addr_base = blockDict['addr_base']
+            if '*' in block:
+                nchannels = self.nInputChannels if '*INPUT' in block else self.nOutputChannels
+                block_names = [block.replace('*','%i'%i) for i in range(nchannels)]
+                for i in range(nchannels):
+                    self.regDict[access][block_names[i]] = {}
+            else:
+                self.regDict[access][block] = {}
+                
+            for param, paramDict in blockDict['registers'].items():
+                addr_offset = paramDict['addr_offset'] if 'addr_offset' in paramDict else 0
+                address = addr_base + addr_offset
+                size_byte = paramDict['size_byte'] if 'size_byte' in paramDict else 1
+                values = paramDict['value'] if 'value' in paramDict else None
+
+                # if this register has parameters for the same address
+                parDict = paramDict['params'] if 'params' in paramDict else {}
+
+                # if not default value given derive it from the default value of the parameters of that register
+                if values is None:
+                    try:
+                        defaults = [reg['param_value'] for reg in param_dict.values()]
+                    except:
+                        print('no default value given of the register or of the parameters of the register')
+                        raise
+                    values = self.__regVal_from_paramValues(parDict)
+                
+                if '*' in param:
+                    try:
+                        for i in range(len(values)):
+                            reg_name = param.replace('*','%i'%i)
+                            reg_addr = address + i*paramDict['addr_shift']
+                            self.regDict[access][block][reg_name] = {'addr': reg_addr,
+                                                                     'val': values[i],
+                                                                     'size_byte': size_byte,
+                                                                     'params': parDict,
+                                                                     }
+                    except:
+                        print('list of parameters but no shift of address')
+                        raise
+                elif '*' in block:
+                    try:
+                        nchannels = self.nInputChannels if '*INPUT' in block else self.nOutputChannels
+                        for i in range(nchannels):
+                            block_name = block.replace('*','%i'%i)
+                            reg_name = param
+                            reg_addr = address + i*block_shift
+                            self.regDict[access][block_name][reg_name] = {'addr': reg_addr,
+                                                                          'val': values,
+                                                                          'size_byte': size_byte,
+                                                                          'params': parDict,
+                                                                          }
+                    except:
+                        print('no block shift in block dictionary')
+                        raise
+                else:
+                    self.regDict[access][block][param] = {'addr': address,
+                                                          'val': values,
+                                                          'size_byte': size_byte,
+                                                          'params': parDict,
+                                                          }
+                    
+        return self.regDict[access]
     
-    def __regVal_from_paramVal(self, reg, param_value, prev_param_value=0):
-        """ Convert parameter value (from config) into register value (1 byte). """
-        reg_value = param_value & reg["param_mask"]
-        reg_value <<= reg["param_shift"]
-        reg_value = prev_param_value + reg_value
+    def __regVal_from_paramValues(self, param_dict, prev_param_value=0):
+        """ Convert parameter values (from config dictionary) into register value. """
+        reg_value = prev_param_value
+        for par, reg in param_dict.items():
+            param_val = (reg["param_value"] & reg["param_mask"])
+            param_val <<= reg["param_shift"]
+            reg_value |= param_val
         return reg_value
 
-    def __paramVal_from_regVal(self, reg, reg_value, prev_reg_value=0):
+    def __paramVal_from_regVal(self, reg, reg_value):
         """ Convert register value into (part of) parameter value. """
         param_val = (reg_value >> reg["param_shift"])
         param_val &= reg["param_mask"]
-        param_val += prev_reg_value
         return param_val
