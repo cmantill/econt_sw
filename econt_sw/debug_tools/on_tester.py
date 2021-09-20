436c420980da32ea8e36a8e0ca5f35714ed2163b
Cristina Mantilla Suarez <criss.ms7@gmail.com>
2021-09-20 16:45:40 +0000

    check point emulator

--- econt_sw/debug_tools/on_tester.py
+++ econt_sw/debug_tools/on_tester.py
@@ -1,31 +1,42 @@
 import uhal
 import time
+import argparse
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('debug', metavar='d', type=str, nargs='+',
+                        help='debug options')
+    parser.add_argument('--io', type=str, default="to",
+                        help='to IO or from IO')
+    args = parser.parse_args()
 
     man = uhal.ConnectionManager("file://connection.xml")
     dev = man.getDevice("mylittlememory")
     uhal.setLogLevelTo(uhal.LogLevel.DEBUG)
 
-    # set invert
-    for link in range(12):
-        dev.getNode("ASIC-IO-IO-to-ECONT-ASIC-IO-blocks-0.link%i.reg0.invert"%link).write(0x1)
-        #dev.getNode("ASIC-IO-IO-to-ECONT-ASIC-IO-blocks-0.link%i.reg0.invert"%link).write(0x0)
-    dev.dispatch()
+    if args.io == "to":
+        io_name = "ASIC-IO-IO-to-ECONT-ASIC-IO-blocks-0"
+        testvectors_switch_name = "test-vectors-to-ASIC-and-emulator-test-vectors-ipif-switch-mux"
+        nlinks = 12
+    else:
+        io_name = "ASIC-IO-IO-from-ECONT-ASIC-IO-blocks-0"
+        nlinks = 13
 
-    # check
-    for link in range(12):
-        bypass = dev.getNode("ASIC-IO-IO-to-ECONT-ASIC-IO-blocks-0.link%i.reg0.bypass_IOBUF"%link).read()
-        dev.dispatch()
-        print('link %i: bypass %d'%(link,bypass))
-
-    # send PRBS
-    for link in range(12):
-        dev.getNode("test-vectors-to-ASIC-and-emulator-test-vectors-ipif-switch-mux.link%i.output_select"%link).write(0x1);
-    dev.dispatch()
-    
-    # send econt link reset
-    '''
-    dev.getNode("fastcontrol_axi.request.link_reset_econt").write(0x1);
-    dev.dispatch()
-    '''
+    for option in args.debug:
+        if option=="IO" or option=="IO-invert":
+            for l in range(nlinks):
+                link = "link%i"%l
+                dev.getNode(io_name+"."+link+".reg0.tristate_IOBUF").write(0x0)
+                dev.getNode(io_name+"."+link+".reg0.bypass_IOBUF").write(0x0)
+                if option=="IO-invert":
+                    dev.getNode(io_name+"."+link+".reg0.invert").write(0x1)
+                else:
+                    dev.getNode(io_name+"."+link+".reg0.invert").write(0x0)
+            dev.dispatch()
+                    
+        if option=="PRBS":
+            for l in range(nlinks):
+                link = "link%i"%l
+                dev.getNode(testvectors_switch_name+"."+link+".output_select").write(0x1)
+            dev.dispatch()
