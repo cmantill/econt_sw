cc75b6d0bdd9f119d0bcedc06db7500bb39c4793
cmantill <criss.ms7@gmail.com>
2021-12-03 23:39:29 +0000

    trying to reduce the use of np

--- econt_sw/testing/uhal-align_on_tester.py
+++ econt_sw/testing/uhal-align_on_tester.py
@@ -1,7 +1,6 @@
 import uhal
 import time
 import argparse
-import numpy as np
 import logging
 logging.basicConfig()
 
@@ -48,41 +47,82 @@ def find_latency(latency,lcapture,bx0=None):
     logger.info('link reset econt counter %i'%lrc) 
 
     # save captured data
+    use_numpy =False
     ASIC_data = get_captured_data(dev,lcapture,nwords=4095,nlinks=output_nlinks)
     # save_testvector("lc-ASIC-findlatency-debug.csv", ASIC_data)
 
     # look for bx0
     BX0_word = 0xf922f922
-    BX0_rows,BX0_cols = (ASIC_data == BX0_word).nonzero()
-
-    try:
-        assert len(BX0_rows) > 0
-        assert (BX0_cols==0).nonzero()
-    except AssertionError:
-        logger.error('BX0 sync word not found anywhere or in link 0')
-        for l in range(output_nlinks):
-            new_latency[l] = -1
-        return new_latency,found_BX0,ASIC_data
-
-    logger.debug('BX0 sync word found on columns %s',BX0_cols)
-    logger.debug('BX0 sync word found on rows %s',BX0_rows)
+    if use_numpy:
+        BX0_rows,BX0_cols = (ASIC_data == BX0_word).nonzero()
+        logger.debug('BX0 sync word found on columns %s',BX0_cols)
+        logger.debug('BX0 sync word found on rows %s',BX0_rows)
+        try: 
+            assert len(BX0_rows) > 0
+            assert (BX0_cols==0).nonzero()
+        except AssertionError:
+            logger.error('BX0 sync word not found anywhere or in link 0')
+            for l in range(output_nlinks):
+                new_latency[l] = -1
+            return new_latency,found_BX0,ASIC_data
+    else:
+        BX0_rows = []
+        BX0_cols = []
+        for i,d in enumerate(ASIC_data):
+            if BX0_word in d:
+                BX0_rows.append(i)
+                for j,col in enumerate(d):
+                    if BX0_word==col and j not in BX0_cols:
+                        BX0_cols.append(j)
+            
+        logger.debug('BX0 sync word found on columns %s',BX0_cols)
+        logger.debug('BX0 sync word found on rows %s',BX0_rows)
+        try:
+            assert len(BX0_rows) > 0
+            assert 0 in BX0_cols 
+            logger.debug('Found BX0 in link 0 ')
+        except AssertionError:
+            logger.error('BX0 sync word not found anywhere or in link 0')
+            for l in range(output_nlinks):
+                new_latency[l] = -1
+                return new_latency,found_BX0,ASIC_data
+        logger.debug('BX0 sync word found on columns %s',BX0_cols)
+        logger.debug('BX0 sync word found on rows %s',BX0_rows)
 
     for l in range(output_nlinks):
-        try:
-            row_index = (BX0_cols==l).nonzero()[0][0]
-            row_link_0 = (BX0_cols==0).nonzero()[0][0]
-            assert BX0_rows[row_index] == BX0_rows[row_link_0]
-            if bx0:
-                assert BX0_rows[row_index] == bx0[row_index]
-            logger.debug('Latency %i: %s found BX0 word at %d',latency[l],lcapture,BX0_rows[row_index])
-            new_latency[l] = latency[l]
-            found_BX0[l] = BX0_rows[row_index]
-        except AssertionError:
-            if bx0:
-                logger.warning('BX0 sync word not found for link %i at (pos of link 0): %i or (pos of where bx0 was found for ASIC): %i'%(l,BX0_rows[row_link_0],bx0[row_index]))
-            else:
-                logger.warning('BX0 sync word not found for link %i at (pos of link 0): %i'%(l,BX0_rows[row_link_0]))
-            new_latency[l] = -1
+        if use_numpy:
+            try:
+                row_index = (BX0_cols==l).nonzero()[0][0]
+                row_link_0 = (BX0_cols==0).nonzero()[0][0]
+                assert BX0_rows[row_index] == BX0_rows[row_link_0]
+                if bx0:
+                    assert BX0_rows[row_index] == bx0[row_index]
+                logger.debug('Latency %i: %s found BX0 word at %d',latency[l],lcapture,BX0_rows[row_index])
+                new_latency[l] = latency[l]
+                found_BX0[l] = BX0_rows[row_index]
+            except AssertionError:
+                if bx0:
+                    logger.warning('BX0 sync word not found for link %i at (pos of link 0): %i or (pos of where bx0 was found for ASIC): %i'%(l,BX0_rows[row_link_0],bx0[row_index]))
+                else:
+                    logger.warning('BX0 sync word not found for link %i at (pos of link 0): %i'%(l,BX0_rows[row_link_0]))
+                new_latency[l] = -1
+        else:
+            try:
+                print(BX0_rows.count(BX0_rows[0]))
+                print(BX0_rows.count(bx0[0]))
+
+                assert BX0_rows.count(BX0_rows[0]) == len(BX0_rows)
+                if bx0:
+                    assert BX0_rows.count(bx0[0]) == len(BX0_rows)
+                logger.debug('Latency %i: %s found BX0 word at %d',latency[l],lcapture,BX0_rows[l])
+                new_latency[l] = latency[l]
+                #found_BX0[l] = BX0_rows[row_index
+            except AssertionError:
+                if bx0:
+                    logger.warning('BX0 sync word not found for link %i at (pos of link 0): %i or (pos of where bx0 was found for ASIC): %i'%(l,BX0_rows[row_link_0],bx0[row_index]))
+                else:
+                    logger.warning('BX0 sync word not found for link %i at (pos of link 0): %i'%(l,BX0_rows[row_link_0]))
+                new_latency[l] = -1
     return new_latency,found_BX0,ASIC_data
         
 if __name__ == "__main__":
@@ -90,7 +130,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-L", "--logLevel", dest="logLevel",action="store",
                         help="log level which will be applied to all cmd : ERROR, WARNING, DEBUG, INFO, NOTICE, NONE",default='NONE')
-    parser.add_argument('--step', choices=['tester-phase','asic-word','asic-tester'], help='alignment steps')
+    parser.add_argument('--step', choices=['test','tester-phase','asic-word','asic-tester'], help='alignment steps')
     
     args = parser.parse_args()
 
@@ -111,13 +151,83 @@ if __name__ == "__main__":
     dev = man.getDevice("mylittlememory")
 
     logger = logging.getLogger('align:step:%s'%args.step)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+
+    if args.step == "test":
+        # configure IO blocks
+        for io in names['IO'].keys():
+            configure_IO(dev,io,io_name='IO')
+            nlinks = input_nlinks if io=='to' else output_nlinks
+            for l in range(nlinks):
+                dev.getNode(names['IO'][io]+".link"+str(l)+".reg0.invert").write(0x1)
+            dev.dispatch()
+
+        # fast command
+        dev.getNode(names['fc']+".command.enable_fast_ctrl_stream").write(0x1);
+        dev.getNode(names['fc']+".command.enable_orbit_sync").write(0x1);
+        dev.getNode(names['fc']+".command.global_l1a_enable").write(0);
+        dev.getNode(names['fc']+".bx_link_reset_roct").write(3500)
+        dev.getNode(names['fc']+".bx_link_reset_rocd").write(3501)
+        dev.getNode(names['fc']+".bx_link_reset_econt").write(3502)
+        dev.getNode(names['fc']+".bx_link_reset_econd").write(3503)
+
+        # configure link captures
+        sync_patterns = {'lc-ASIC': 0x122,
+                         'lc-emulator': 0x122,
+                         'lc-input': 0xaccccccc,
+                     }
+        for lcapture in ['lc-input','lc-ASIC','lc-emulator']:
+            dev.getNode(names[lcapture]['lc']+".global.link_enable").write(0x1fff)
+            dev.getNode(names[lcapture]['lc']+".global.explicit_resetb").write(0x0)
+            time.sleep(0.001)
+            dev.getNode(names[lcapture]['lc']+".global.explicit_resetb").write(0x1)
+            dev.dispatch()
+            nlinks = input_nlinks if 'input' in lcapture else output_nlinks
+            for l in range(nlinks):
+                dev.getNode(names[lcapture]['lc']+".link"+str(l)+".align_pattern").write(sync_patterns[lcapture])
+                dev.getNode(names[lcapture]['lc']+".link"+str(l)+".fifo_latency").write(0);
+                dev.dispatch()
+
+        for lcapture in ['lc-ASIC','lc-emulator']:
+            # configure link captures to acquire on linkreset-ECONt (4095 words)
+            configure_acquire(dev,lcapture,"linkreset_ECONt",nwords=4095,nlinks=output_nlinks)
+
+        # send link reset econt (once)
+        lrc = dev.getNode(names['fc-recv']+".counters.link_reset_econt").read();
+        dev.dispatch()
+        logger.info('link reset econt counter %i'%lrc)
+
+        dev.getNode(names['fc']+".bx_link_reset_econt").write(3550)
+        dev.dispatch()
+        dev.getNode(names['fc']+".request.link_reset_econt").write(0x1);
+        dev.dispatch()
+        time.sleep(0.1)
+
+        lrc = dev.getNode(names['fc-recv']+".counters.link_reset_econt").read();
+        dev.dispatch()
+        logger.info('link reset econt counter %i'%lrc)
+
+        # capture data                                                                                                                                                                                  
+        raw_input("Need to capture data in output. Press key to continue...")
+        do_fc_capture(dev,"link_reset_econt",'lc-ASIC')
+        time.sleep(0.001)
+        lrc = dev.getNode(names['fc-recv']+".counters.link_reset_econt").read();
+        dev.dispatch()
+        logger.info('link reset econt counter %i'%lrc)
+        data = get_captured_data(dev,'lc-ASIC',nwords=4095,nlinks=output_nlinks)
+        save_testvector("lc-ASIC-alignoutput_debug.csv", data)
+        raw_input("Sent link reset ECONT. Press key to continue...")
+        exit(1)
     
     if args.step == "tester-phase":
         # configure IO blocks
         for io in names['IO'].keys():
             configure_IO(dev,io,io_name='IO')
-        
+            nlinks = input_nlinks if io=='to' else output_nlinks
+            for l in range(nlinks):
+                dev.getNode(names['IO'][io]+".link"+str(l)+".reg0.invert").write(0x1)        
+            dev.dispatch()
+
         # send PRBS
         testvectors_settings = {
             "output_select": 0x1,
@@ -136,6 +246,32 @@ if __name__ == "__main__":
             dev.getNode(names['testvectors']['stream']+".link"+str(l)+".force_sync").write(0x0)
         dev.dispatch()
         raw_input("IO blocks configured. Sending PRBS. Press key to continue...")
+
+        # configure fast command
+        dev.getNode(names['fc']+".command.enable_fast_ctrl_stream").write(0x1);
+        dev.getNode(names['fc']+".command.enable_orbit_sync").write(0x1);
+        dev.getNode(names['fc']+".command.global_l1a_enable").write(0);
+
+        # configure BXs
+        dev.getNode(names['fc']+".bx_link_reset_roct").write(3500)
+        dev.getNode(names['fc']+".bx_link_reset_rocd").write(3501)
+        dev.getNode(names['fc']+".bx_link_reset_econt").write(3502)
+        dev.getNode(names['fc']+".bx_link_reset_econd").write(3503)
+        
+        # send link reset ECONT
+        lrc = dev.getNode(names['fc-recv']+".counters.link_reset_roct").read();
+        dev.dispatch()
+        logger.info('link reset roct counter %i'%lrc)
+
+        dev.getNode(names['fc']+".request.link_reset_roct").write(0x1);
+        dev.dispatch()
+        time.sleep(2)
+
+        lrc = dev.getNode(names['fc-recv']+".counters.link_reset_roct").read();
+        dev.dispatch()
+        logger.info('link reset roct counter %i'%lrc)
+
+        raw_input("Sent link reset ROCT. Press key to continue...")
 
         # check that from-IO is aligned
         isIO_aligned = check_IO(dev,io='from',nlinks=output_nlinks)
@@ -255,13 +391,14 @@ if __name__ == "__main__":
             # capture data
             raw_input("Need to capture data in output. Press key to continue...")
             do_fc_capture(dev,"link_reset_econt",'lc-ASIC')
+            time.sleep(0.001)
             lrc = dev.getNode(names['fc-recv']+".counters.link_reset_econt").read();
             dev.dispatch()
             logger.info('link reset econt counter %i'%lrc)
             data = get_captured_data(dev,'lc-ASIC',nwords=4095,nlinks=output_nlinks)
             save_testvector("lc-ASIC-alignoutput_debug.csv", data)
             raw_input("Sent link reset ECONT. Press key to continue...")
-            #exit(1)
+            exit(1)
 
         # data to be captured
         all_data = {}
