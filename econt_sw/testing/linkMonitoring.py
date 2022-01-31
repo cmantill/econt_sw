e7bd3b1833ea17489e79153308de69f775abfaf8
dnoonan08 <dnoonan@cern.ch>
2022-01-31 23:04:44 +0000

    add prbs scan

--- econt_sw/testing/linkMonitoring.py
+++ econt_sw/testing/linkMonitoring.py
@@ -1,6 +1,7 @@
 from i2c import call_i2c
 import numpy as np
 import argparse
+import os
 
 def readSnapshot(i2c='ASIC',return_status=False):
     x=call_i2c(args_yaml='configs/align_read.yaml', args_i2c=i2c)
@@ -110,16 +111,60 @@ def statusLogging(sleepTime=120, N=30):
 
 
 
+
+def prbsScan(sleepTime=5, threshold=0, verbose=False):
+    os.system('python testing/uhal/align_on_tester.py --step test-data --dtype PRBS')
+
+    call_i2c(args_name=f'EPRXGRP_TOP_trackMode',args_value='0')
+
+    errCounts=[]
+    for i in range(16):
+        call_i2c(args_name='CH_ALIGNER_[0-11]_prbs_chk_en', args_value='[0]*12')
+        call_i2c(args_name=f'MISC_rw_ecc_err_clr',args_value='1')
+        call_i2c(args_name='CH_EPRXGRP_[0-11]_phaseSelect',args_value=f'{i}')
+        call_i2c(args_name='CH_ALIGNER_[0-11]_prbs_chk_en', args_value='[1]*12')
+        call_i2c(args_name=f'MISC_rw_ecc_err_clr',args_value='0')
+        sleep(sleepTime)
+        x=call_i2c(args_name=f'CH_ALIGNER_[0-11]_prbs_chk_err_cnt')
+        errCounts.append([x['ASIC']['RO'][f'CH_ALIGNER_{j}INPUT_ALL']['prbs_chk_err_cnt'] for j in range(12)])
+        if verbose:
+            print(' phaseSelect: {:02n}, prbs_chk_err_cnt: {}'.format(i,str(errCounts[-1])))
+    errCounts = np.array(errCounts).astype(int)
+    if verbose:
+        print('PhaseScanOutputs:')
+        print(errCounts)
+
+    countsWindow=[]
+    for i in range(15):
+        #add counts over 3 setting window, summing i, i+1, and i-1 (mod 15)
+        countsWindow.append( errCounts[i] + errCounts[(i-1)%15] + errCounts[(i+1)%15] )
+    countsWindow=np.array(countsWindow)
+
+    if verbose:
+        print()
+        print('Error Counts over 3 setting window:')
+        print(countsWindow)
+    countsWindow[ errCounts[:-1]>threshold ] = 255*3
+    bestSetting=np.array(countsWindow).argmin(axis=0)
+    if verbose:
+        print()
+        print('Best phase settings: ', bestSetting)
+    return errCounts, bestSetting
+
+
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--logging', dest='runLogging',default=False, action='store_true')
     parser.add_argument('--snapshot', dest='getSnapshot',default=False, action='store_true')
     parser.add_argument('--hdrMM', dest='getHdrMM',default=False, action='store_true')
+    parser.add_argument('--prbs', dest='prbsPhaseScan',default=False, action='store_true')
     parser.add_argument('--alignment', dest='checkWordAlignment',default=False, action='store_true')
     parser.add_argument('-N',dest='N',default=1,type=int,help='Number of iterations to run')
     parser.add_argument('--sleep',dest='sleepTime',default=120,type=int,help='Time to wait between logging iterations')
     parser.add_argument('--verbose', dest='verbose',default=False, action='store_true')
+    parser.add_argument('--threshold', dest='threshold',default=0,type=int, action='Threshold of number of allowed errors')
 
     args = parser.parse_args()
 
@@ -136,3 +181,6 @@ if __name__=='__main__':
         x=get_HDR_MM_CNTR()
         print(x)
 
+    if args.prbsPhaseScan:
+        x=prbsScan(sleepTime=args.sleepTime, threshold=args.threshold, verbose=args.verbose)
+        
