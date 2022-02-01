1a63581a1ccad28e9ada5047d074921d79e08e26
cmantill <criss.ms7@gmail.com>
2022-02-01 21:42:29 +0000

    set tx sync word just in case

--- econt_sw/scripts/lcAlignment.sh
+++ econt_sw/scripts/lcAlignment.sh
@@ -1,5 +1,6 @@
 echo "Starting link capture alignment"
 echo "     send link reset ECONT"
+python3 testing/i2c.py --name FMTBUF_tx_sync_word --value 0x122
 python testing/uhal/align_on_tester.py --step lr-econt
 python testing/uhal/check_align.py --check --block lc-ASIC
 python testing/uhal/align_on_tester.py --step capture --lc lc-ASIC --mode linkreset_ECONt
