89f156fe469cbbc2c42218e6abc1593eab37e611
cmantill <criss.ms7@gmail.com>
2022-02-15 23:14:31 +0000

    allow for different select value

--- econt_sw/scripts/quickASICSetup.sh
+++ econt_sw/scripts/quickASICSetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ASIC="${1:-2}"
-GOODPHASEPLL="${1:-0}"
+GOODPHASEPLL="${2:-0}"
+GOODSELECT="${3:-56}"
 
 python3 testing/i2c.py --name FCTRL_EdgeSel_T1 --value 0 --quiet
 python3 testing/i2c.py --name PLL_ref_clk_sel,PLL_enableCapBankOverride,PLL_CBOvcoCapSelect --value 1,1,100 --quiet
@@ -13,7 +14,7 @@ if [ $ASIC -eq 3 ]; then
     python3 testing/i2c.py --name EPRXGRP_TOP_trackMode,CH_EPRXGRP_[0-11]_phaseSelect --value 0,6,13,7,14,14,0,7,8,7,7,7,8 --quiet
 fi
 
-python3 testing/i2c.py --name CH_ALIGNER_[0-11]_sel_override_val,CH_ALIGNER_[0-11]_sel_override_en,ALIGNER_orbsyn_cnt_load_val --value [0x38]*12,[1]*12,0 --quiet
+python3 testing/i2c.py --name CH_ALIGNER_[0-11]_sel_override_val,CH_ALIGNER_[0-11]_sel_override_en,ALIGNER_orbsyn_cnt_load_val --value [${GOODSELECT}]*12,[1]*12,0 --quiet
 
 python3 testing/i2c.py --name ALGO_threshold_val_[0-47],FMTBUF_eporttx_numen --value [0x3fffff]*48,13 --quiet
 python3 testing/i2c.py --name FMTBUF_tx_sync_word,ALGO_drop_lsb --value 0,1 --quiet
