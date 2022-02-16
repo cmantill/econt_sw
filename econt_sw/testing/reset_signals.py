86debd5a9b760e8173c914d6307d4f8f9e0bc301
dnoonan08 <dnoonan@cern.ch>
2022-02-16 23:06:59 +0000

    make class, so when imported into another script the device is loaded only once

--- econt_sw/testing/reset_signals.py
+++ econt_sw/testing/reset_signals.py
@@ -12,6 +12,66 @@ Setting reset signals with uHal
 Usage: python reset_signals.py --i2c ASIC --reset hard --release
 """
 
+class ResetSignals:
+    """
+    Class for handling uhal reset signals
+    """
+
+    def __init__(self):
+        """Initialization class to setup connection manager and device"""
+        self.man = uhal.ConnectionManager("file://connection.xml")
+        self.dev = self.man.getDevice("mylittlememory")
+
+    def send_reset(self, reset='soft',i2c='ASIC', hold=False, release=False, sleepTime=0.5):
+        """Send reset signal to device (either ASIC or emulator), either soft or hard reset
+        """
+        if reset=='hard':
+            # hard reset: go to state sleep mode, reset entire chip and all i2c is cleared
+            reset_string = "ECONT_%s_RESETB"%i2c
+        elif reset=='soft':
+            # soft reset: same as hard reset but leaves i2c programmed
+            reset_string = "ECONT_%s_SOFT_RESETB"%i2c
+        else:
+            logger.Error('No reset signal provided')
+
+        if hold:
+            self.dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(0)
+            self.dev.dispatch()
+        elif release:
+            self.dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(1)
+            self.dev.dispatch()
+        else:
+            self.dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(0)
+            self.dev.dispatch()
+            sleep(sleepTime)
+            self.dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(1)
+            self.dev.dispatch()
+
+    def repeat_reset(self,reset='soft',i2c='ASIC', sleepTime=0.5, N=2):
+        """Send repeated reset signals N times"""
+        for i in range(N):
+            self.send_reset(reset=reset,
+                            i2c=i2c,
+                            sleepTime=sleepTime)
+            sleep(sleepTime)
+
+    def read(self,reset='soft',i2c='ASIC',verbose=True):
+        """Read current state of reset line"""
+        if reset=='hard':
+            # hard reset: go to state sleep mode, reset entire chip and all i2c is cleared
+            reset_string = "ECONT_%s_RESETB"%i2c
+        elif reset=='soft':
+            # soft reset: same as hard reset but leaves i2c programmed
+            reset_string = "ECONT_%s_SOFT_RESETB"%i2c
+        else:
+            logger.Error('No reset signal provided')
+
+        resetStatus = self.dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).read()
+        self.dev.dispatch()
+        if verbose:
+            print(reset_string, int(resetStatus))
+        return resetStatus
+
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser(description='Align links')
     parser.add_argument("-L", "--logLevel", dest="logLevel",action="store",
@@ -32,40 +92,19 @@ if __name__ == "__main__":
     except ValueError:
         logging.error("Invalid log level")
         exit(1)
-    man = uhal.ConnectionManager("file://connection.xml")
-    dev = man.getDevice("mylittlememory")
-    
-    if args.reset=='hard':
-        # hard reset: go to state sleep mode, reset entire chip and all i2c is cleared
-        reset_string = "ECONT_%s_RESETB"%args.i2c
-    elif args.reset=='soft':
-        # soft reset: same as hard reset but leaves i2c programmed
-        reset_string = "ECONT_%s_SOFT_RESETB"%args.i2c
-    else:
-        logger.Error('No reset signal provided')
 
-    if args.release:
-        dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(1)
-        dev.dispatch()
-    elif args.hold:
-        dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(0)
-        dev.dispatch()
+    resets=ResetSignals()
+    if args.repeat:
+        resets.repeat_reset(reset=args.reset,
+                            i2c=args.i2c,
+                            sleepTime=args.time,
+                            N=args.repeat)
     elif args.read:
-        reset = dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).read()
-        dev.dispatch()
-        print(reset_string, int(reset))
-    elif args.repeat:
-        for i in range(args.repeat):
-            print(i)
-            dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(0)
-            dev.dispatch()
-            sleep(args.time)
-            dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(1)
-            dev.dispatch()
-            sleep(args.time)
+        resets.read(reset=args.reset,
+                    i2c=args.i2c)
     else:
-        dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(0)
-        dev.dispatch()
-        sleep(args.time)
-        dev.getNode("ASIC-IO-I2C-I2C-fudge-0.resets.%s"%reset_string).write(1)
-        dev.dispatch()
+        resets.send_reset(reset=args.reset,
+                          i2c=args.i2c,
+                          hold=args.hold,
+                          release=args.release,
+                          sleepTime=args.time)
