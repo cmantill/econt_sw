1e125230121d47b45ce1d02750cbf6ceee2d1356
cmantill <criss.ms7@gmail.com>
2022-02-21 18:45:28 +0000

    correct request fc

--- econt_sw/testing/fast_command.py
+++ econt_sw/testing/fast_command.py
@@ -1,40 +1,31 @@
-import uhal
 import argparse
+
 import logging
 logging.basicConfig()
+logger = logging.getLogger('fc')
+logger.setLevel('INFO')
 
 from time import sleep
 
 from utils.fast_command import FastCommands
-from utils.uhal_config import *
 
 """
 Send/Read fast commands
 """
 
 if __name__ == "__main__":    
-    parser = argparse.ArgumentParser(description='Align links')
-    parser.add_argument("-L", "--logLevel", dest="logLevel",action="store",
-                        help="log level which will be applied to all cmd : ERROR, WARNING, DEBUG, INFO, NOTICE",default='INFO')
+    parser = argparse.ArgumentParser(description='Fast commands')
     parser.add_argument('--fc', type=str, required=True, 
                         choices=['chipsync','command-delay'],
                         help='fast command')
     parser.add_argument('--read', action='store_true', default=False, help='read')
     args = parser.parse_args()
 
-    set_logLevel(args)
-    logger = logging.getLogger('fc')
-    try:
-        logger.setLevel(args.logLevel)
-    except ValueError:
-        logging.error("Invalid log level")
-        exit(1)
-
     fc=FastCommands()
     fc.configure_fc(args.read)
     
     if args.fc=='chipsync':
-        fc.chipsync()
+        fc.request('chipsync')
     elif args.fc=='command-delay':
         if args.read:
             fc.read_command_delay()
