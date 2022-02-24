e965b58cf1a46df735752873e1202b0465f29ac3
cmantill <criss.ms7@gmail.com>
2022-02-24 15:22:56 +0000

    guarantee that sync word is the usual one for io alignment

--- econt_sw/scripts/ioAlignment.sh
+++ econt_sw/scripts/ioAlignment.sh
@@ -2,9 +2,9 @@ ALGO="${1:-0}"
 THRESHOLD="${2:-0x3fffff}"
 python testing/i2c.py --name MFC_ALGORITHM_SEL_DENSITY_algo_select --value $ALGO --i2c ASIC,emulator --quiet
 python testing/i2c.py --name ALGO_threshold_val_[0-47] --value $THRESHOLD --i2c ASIC,emulator --quiet
-python testing/i2c.py --name FMTBUF_eporttx_numen --value 13
+python testing/i2c.py --name FMTBUF_eporttx_numen,FMTBUF_tx_sync_word --value 13,0x122 --i2c ASIC,emulator --quiet
 
-python testing/align_on_tester.py --step configure-IO --invertIO
 python testing/eRx.py --tv --dtype zeros
+python testing/align_on_tester.py --step configure-IO --invertIO
 python testing/check_block.py --check --block from-IO
-python testing/align_on_tester.py --step manual-IO
+#python testing/align_on_tester.py --step manual-IO
