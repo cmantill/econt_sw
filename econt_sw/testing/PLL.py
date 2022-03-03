35cea0c8b3b84dcb44c6a6e9a3901bcc90d5070d
cmantill <criss.ms7@gmail.com>
2022-03-03 20:25:07 +0000

    pll counter from Danny

---
+++ econt_sw/testing/PLL.py
@@ -0,0 +1,24 @@
+import logging
+logger = logging.getLogger("pll")
+logger.setLevel(logging.INFO)
+
+from utils.pll_lock_count import PLLLockCount
+
+pll=PLLLockCount()
+
+def get_count():
+    logger.info('Loss of lock count ',pll.getCount())
+    logger.info('Edge to count ',pll.edgeSel(read=True))
+    pll.edgeSel(val=1)
+    logger.info('Edge to count ',pll.edgeSel(read=True))
+    pll.edgeSel(val=0)
+    logger.info('Edge to count ',pll.edgeSel(read=True))
+    logger.info('Loss of lock count ',pll.getCount())
+    
+    # reset counters
+    pll.reset()
+    
+    logger.info('Loss of lock count ',pll.getCount())
+
+if __name__=='__main__':
+    get_count()
---
+++ econt_sw/testing/utils/pll_lock_count.py
@@ -0,0 +1,42 @@
+import os
+import uhal
+from .uhal_config import set_logLevel
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger('utils:pll_lock')
+logger.setLevel(logging.INFO)
+from time import sleep
+
+class PLLLockCount:
+    """Class to monitor PLL_LOCK_B transition counters on FPGA"""
+
+    def __init__(self,logLevel=""):
+        """Initialization class to setup connection manager and device"""
+        set_logLevel(logLevel)
+
+        self.man = uhal.ConnectionManager("file://connection.xml")
+        self.dev = self.man.getDevice("mylittlememory")
+        self.name = "ASIC-IO-Extra-IO-AXI-edge-counter-0"
+
+    def getCount(self):
+        PLL_switch_count = self.dev.getNode(self.name+".counter").read()
+        self.dev.dispatch()
+        return int(PLL_switch_count)
+
+    def edgeSel(self,read=False,val=0):
+        if read:
+            edgeSelVal = self.dev.getNode(self.name+".edge_select").read()
+            self.dev.dispatch()
+            return int(edgeSelVal)
+        else:
+            self.dev.getNode(self.name+".edge_select").write(int(val))
+            self.dev.dispatch()
+        return
+    
+    def reset(self):
+        self.dev.getNode(self.name+".clear_counter").write(1);
+        self.dev.dispatch()
+        sleep(1)
+        self.dev.getNode(self.name+".clear_counter").write(0);
+        self.dev.dispatch()
