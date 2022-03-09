8274ffecfe678c3a5f45cd3f51d9d516a10c245f
cmantill <criss.ms7@gmail.com>
2022-03-09 18:48:30 +0000

    set nch 13

--- econt_sw/scripts/lcAlignment.sh
+++ econt_sw/scripts/lcAlignment.sh
@@ -1,6 +1,6 @@
 echo "Starting link capture alignment"
 echo "     send link reset ECONT"
-python testing/i2c.py --name FMTBUF_tx_sync_word --value 0x122
+python testing/i2c.py --name FMTBUF_tx_sync_word,FMTBUF_eporttx_numen --value 0x122,0xd
 python testing/align_on_tester.py --step lr-econt
 python testing/check_block.py --check --block lc-ASIC
 python testing/eTx.py --capture --lc lc-ASIC --mode linkreset_ECONt --capture --csv
