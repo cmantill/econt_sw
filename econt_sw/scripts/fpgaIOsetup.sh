dc3d53666b69a273434a785b53e1c444659eda6e
cmantill <criss.ms7@gmail.com>
2022-04-21 11:25:14 -0500

    remove asic from fpga config

--- econt_sw/scripts/fpgaIOsetup.sh
+++ econt_sw/scripts/fpgaIOsetup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-ASIC="${1:-8}"
-
 source env.sh
-echo "Configure IO for board " ${ASIC}
+echo "Configure IO"
 python testing/align_on_tester.py --step configure-IO --invertIO
 
 echo "Initialize FC and send 28 bit PRBS data"
--- econt_sw/scripts/setup.sh
+++ econt_sw/scripts/setup.sh
@@ -5,7 +5,7 @@ GOODSELECT="${3:-56}"
 PLLCAP="${4:-15}"
 
 source scripts/ASICsetup.sh ${ASIC} ${GOODPHASEPLL} ${GOODSELECT} ${PLLCAP}
-source scripts/fpgaIOsetup.sh ${ASIC}
+source scripts/fpgaIOsetup.sh
 source scripts/inputWordAlignment.sh 3 3
 source scripts/ioAlignment.sh
 source scripts/lcAlignment.sh
