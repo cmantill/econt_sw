782ada2e8e54e9e4229f0c8527ac9d43368f37da
cmantill <criss.ms7@gmail.com>
2022-04-26 12:50:15 -0500

    working alignment script

--- econt_sw/scripts/bypass.sh
+++ econt_sw/scripts/bypass.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 IDIR=$1
 TAG=$2
-LATENCY="${3:-12}"
+LATENCY="${3:-13}"
 BCR="${4:-1}"
 python testing/eRx.py --tv --idir ${IDIR} --tv-name bypass --fname testOutput.csv
 python testing/align_on_tester.py --step mlatency --lat ${LATENCY}
 python testing/i2c.py --name ALIGNER_orbsyn_cnt_load_val --value ${BCR}
-python testing/eTx.py --daq --idir ${IDIR} --nocompare
 python testing/i2c.py --name MISC_run --value 0
-python testing/i2c.py --name MISC_run --value 1
-python testing/eTx.py --daq --odir ${TAG}_bypass_bcr${BCR}_lat${LATENCY} --trigger --i2ckeep
+python testing/eTx.py --daq --idir ${IDIR} --nocompare
+#python testing/fast_command.py --fc chipsync
+#python testing/fast_command.py --fc link_reset_econt
+python testing/i2c.py --name MISC_run --value 1 
+python testing/eTx.py --daq --idir ${IDIR} --odir ${TAG}_bypass_bcr${BCR}_lat${LATENCY} --trigger --i2ckeep
 python testing/eTx.py --capture --lc lc-input,lc-ASIC,lc-emulator --mode BX --bx 3500 --capture --nwords 100 --csv --odir ${TAG}_bypass_bcr${BCR}_lat${LATENCY}
+
