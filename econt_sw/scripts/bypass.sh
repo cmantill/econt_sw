52b9e83a95a6b685ea2282d9d3f1f465803eb586
cmantill <criss.ms7@gmail.com>
2022-05-02 15:31:33 -0500

    remove chip sync from bypass

--- econt_sw/scripts/bypass.sh
+++ econt_sw/scripts/bypass.sh
@@ -8,8 +8,6 @@ python testing/align_on_tester.py --step mlatency --lat ${LATENCY}
 python testing/i2c.py --name ALIGNER_orbsyn_cnt_load_val --value ${BCR}
 python testing/i2c.py --name MISC_run --value 0
 python testing/eTx.py --daq --idir ${IDIR} --nocompare
-#python testing/fast_command.py --fc chipsync
-#python testing/fast_command.py --fc link_reset_econt
 python testing/i2c.py --name MISC_run --value 1 
 python testing/eTx.py --daq --idir ${IDIR} --odir ${TAG}_bypass_bcr${BCR}_lat${LATENCY} --trigger --i2ckeep
 python testing/eTx.py --capture --lc lc-input,lc-ASIC,lc-emulator --mode BX --bx 3500 --capture --nwords 100 --csv --odir ${TAG}_bypass_bcr${BCR}_lat${LATENCY}
