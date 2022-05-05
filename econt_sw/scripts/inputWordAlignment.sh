940478587218f54645fb2248d63bd884e58854f5
cmantill <criss.ms7@gmail.com>
2022-05-05 11:26:20 -0500

    do not use prbs for input word alignment

--- econt_sw/scripts/inputWordAlignment.sh
+++ econt_sw/scripts/inputWordAlignment.sh
@@ -3,7 +3,6 @@ EMULATOR_DELAY="${2:-4}"
 SELECT="${2:-32}"
 
 echo "Starting word alignment"
-python testing/eRx.py --tv --dtype PRBS28
 python testing/eRx.py --lrAlign --bx $SNAPSHOT --bcr 0 --delay $EMULATOR_DELAY --verbose
 python testing/eRx.py --logging -N 1 --sleep 2
 
