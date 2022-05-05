4c13f14a9130b5ea7d2be45cfc45dbe12b487846
cmantill <criss.ms7@gmail.com>
2022-05-05 11:16:16 -0500

    simplify logging in alignment

--- econt_sw/scripts/inputWordAlignment.sh
+++ econt_sw/scripts/inputWordAlignment.sh
@@ -1,12 +1,10 @@
-SNAPSHOT="${1:-3}"
-EMULATOR_DELAY="${2:-3}"
+SNAPSHOT="${1:-4}"
+EMULATOR_DELAY="${2:-4}"
 SELECT="${2:-32}"
 
 echo "Starting word alignment"
 python testing/eRx.py --tv --dtype PRBS28
-python testing/eRx.py --lrAlign --bx $SNAPSHOT --bcr 0 --delay $EMULATOR_DELAY
+python testing/eRx.py --lrAlign --bx $SNAPSHOT --bcr 0 --delay $EMULATOR_DELAY --verbose
+python testing/eRx.py --logging -N 1 --sleep 2
 
 #python testing/eRx.py --override --select $SELECT
-
-python testing/eRx.py --checkAlign --verbose
-python testing/eRx.py --logging -N 1 --sleep 2 
