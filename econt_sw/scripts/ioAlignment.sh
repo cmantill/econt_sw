cd2462ec1432dabdbbad15582e2889a268e64dbb
Danny Noonan <dnoonan@cern.ch>
2022-05-24 17:26:38 -0500

    update startup scripts

--- econt_sw/scripts/ioAlignment.sh
+++ econt_sw/scripts/ioAlignment.sh
@@ -1,9 +1,9 @@
-ALGO="${1:-0}"
-THRESHOLD="${2:-0x3fffff}"
-python testing/i2c.py --name MFC_ALGORITHM_SEL_DENSITY_algo_select --value $ALGO --i2c ASIC,emulator --quiet
-python testing/i2c.py --name ALGO_threshold_val_[0-47] --value $THRESHOLD --i2c ASIC,emulator --quiet
-python testing/i2c.py --name FMTBUF_eporttx_numen,FMTBUF_tx_sync_word --value 13,0x122 --i2c ASIC,emulator --quiet
+# ALGO="${1:-0}"
+# THRESHOLD="${2:-0x3fffff}"
+# echo ${ALGO}
+# echo ${THRESHOLD}
 
+python testing/i2c.py --name MFC_ALGORITHM_SEL_DENSITY_algo_select,ALGO_threshold_val_[0-47],FMTBUF_eporttx_numen,FMTBUF_tx_sync_word --value 0,[0x3fffff]*48,13,0x122 --i2c ASIC,emulator --quiet
 python testing/align_on_tester.py --step configure-IO --invertIO
 python testing/check_block.py --check --block from-IO
 python testing/align_on_tester.py --step manual-IO
--- econt_sw/scripts/setup.sh
+++ econt_sw/scripts/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ASIC="${1:-8}"
-GOODPHASEPLL="${2:-0}"
-GOODSELECT="${3:-56}"
-PLLCAP="${4:-15}"
+SNAPSHOTBX="${2:-2}"
+DELAY="${3:-1}"
+GOODPHASEPLL="${4:-0}"
 
-source scripts/ASICsetup.sh ${ASIC} ${GOODPHASEPLL} ${GOODSELECT} ${PLLCAP}
+source scripts/ASICsetup.sh ${ASIC} ${GOODPHASEPLL}
 source scripts/fpgaIOsetup.sh
-source scripts/inputWordAlignment.sh
+source scripts/inputWordAlignment.sh ${SNAPSHOTBX} ${DELAY}
 source scripts/ioAlignment.sh
 source scripts/lcAlignment.sh
 source scripts/lcEmulatorAlignment.sh
